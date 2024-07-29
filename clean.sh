00f3b2bcdb2ab2e45669b7e25830d62ff4c74abe
AlbanAndrieu <alban.andrieu@free.fr>
2024-07-29 19:46:26 +0200

    Fix tox clean

--- clean.sh
+++ clean.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #set -xv
 
-rm -Rf _build/ build/ .eggs/ .toxs/ dist/ output/pytest-report.xml .coverage output/coverage.xml docs/_build/ .tox/ .scannerwork/ .pytest_cache/ output/htmlcov/ cprofile
-rm -Rf pytest-report.xml coverage.xml
+rm -Rf _build/ build/ .eggs/ .toxs/ dist/ output/pytest-report.xml .coverage output/coverage.xml coverage.xml docs/_build/ .tox/ .scannerwork/ .pytest_cache/ pytest-report.xml output/htmlcov/ cprofile
 
 find . -maxdepth 2 -mindepth 2 -regextype posix-egrep -type d -regex '.+/.*egg-info' -exec rm -rf {} \;
 find . -maxdepth 2 -mindepth 2 -regextype posix-egrep -type d -regex '.*__pycache__.*' -exec rm -rf {} \;
